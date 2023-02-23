30bbe615ac742e722c754db9904ecad3a60e396d
Duane <duanerichard47@hotmail.com>
2023-02-23 18:53:52 -0500

    incomplete

--- lib/book.py
+++ lib/book.py
@@ -1,4 +1,66 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+
+    def __init__(self, title):
+        self.title = title
+        self.genre = genre
+
+    def get_page_count(self):
+        return self._page_count
+
+        
+    def set_page_count(self, page_count):
+        if type(page_count) != int:
+            print("page_count must be an integer")
+        else:
+         self._page_count = page_count
+
+    def turn_pages(self):
+        print("Flipping the page...wow, you read fast!")
+
+    page_count = property(get_page_count, set_page_count)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+""" class Book:
+    def __init__(self, title):
+        self.title = title
+        self._page_count = None
+
+    def get_page_count(self):
+        return self._page_count
+
+    def set_page_count(self, new_page_count):
+        if type(new_page_count) != int:
+            print('new page count must be an integer')
+        else:
+            self._page_count = new_page_count
+
+    page_count = property(get_page_count, set_page_count)
+
+    def turn_page(self)
+
+
+if __name__=='__main__':
+    book = Book('The Road') """
+
+
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,80 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, color, size, material, condition):
+        self.brand = brand
+        self.color = color
+        self.size = size
+        self.material = material
+        self.condition = condition
+
+
+    def get_page_count(self):
+        return self._page_count
+    
+    def set_page_count(self, page_count):
+        if type(page_count) != int:
+            print("page_count must be an integer")
+        else:
+         self._page_count = page_count
+
+    page_count = property(get_page_count, set_page_count)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class Shoe:
+#     def __init__ (self, brand):
+#       self.brand = brand
+#       self.color = None
+#       self.size = None
+#       self.condition = None
+
+#     def set_size(self, new_size):
+#        if type(new_size) != int:
+#           print('idk')
+#        else:
+#           self._size = new_size
+      
+#     def get_size(self):
+       
+#     size = property(get_size, set_size)
+
+#     def cobble(self):
+#        self.condition
+      
